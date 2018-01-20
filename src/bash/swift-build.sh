725d4763ea89cbe477cc9f84bc2da5585be18f9c
Anton Pogonets <apogonets@readdle.com>
2018-01-20 02:06:58 +0200

    Workaround sandboxing. Move dynamic tools lookup into swift-build

--- src/bash/swift-build.sh
+++ src/bash/swift-build.sh
@@ -9,6 +9,12 @@ export PATH="$SWIFT_ANDROID_HOME/toolchain/usr/bin:$PATH"
 export CC="$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/darwin-x86_64/bin/clang"
 export SWIFT_EXEC=$SELF_DIR/swiftc-android.sh
 
+# all dynamic lookup should be done here
+# because of strange sandbox like environment
+# inside Swift PM called scripts
+export _XCODE_SWIFT=$(xcrun --find swift)
+export _XCODE_TOOLCHAIN=$(dirname $(dirname $(dirname $_XCODE_SWIFT)))
+
 include=-I.build/jniLibs/include
 libs=-L.build/jniLibs/armeabi-v7a
 
--- src/bash/swiftc-android.sh
+++ src/bash/swiftc-android.sh
@@ -3,11 +3,13 @@
 : "${ANDROID_NDK_HOME:?$ANDROID_NDK_HOME should be set}"
 
 if [[ "$*" =~ " -fileno " ]]; then
+    _XCODE_SWIFT=${_XCODE_SWIFT:-$(xcrun --find swift)}
+    _XCODE_TOOLCHAIN=${_XCODE_TOOLCHAIN:-$(dirname $(dirname $(dirname $_XCODE_SWIFT)))}
+
     # remove trailing slashes
     SWIFT_ANDROID_HOME=$(echo $SWIFT_ANDROID_HOME | sed -e 's%/*$%%')
 
-    xcode_toolchain=$(dirname $(dirname $(dirname $(xcrun --find swiftc))))
-    swift_pm_runtime=$xcode_toolchain/usr/lib/swift/pm
+    swift_pm_runtime=$_XCODE_TOOLCHAIN/usr/lib/swift/pm
 
     # fix paths to Swift PM runtime keeping args
     args=( "$@" )
@@ -16,7 +18,7 @@ if [[ "$*" =~ " -fileno " ]]; then
         args[$i]=$(echo "${args[$i]}" | sed -E "s%$SWIFT_ANDROID_HOME/toolchain/usr/lib/swift/pm%$swift_pm_runtime%g")
     done
 
-    swift "${args[@]}" || { 
+    $_XCODE_SWIFT "${args[@]}" || {
         return_code=$? 
         echo "*** Error executing: $0 $@"
         exit $return_code
