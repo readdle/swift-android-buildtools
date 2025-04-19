cd4492c1a0c6747e3f214de3f844df959c525e72
Andrew Druk <adruk@readdle.com>
2025-04-19 13:09:01 +0300

    Dev: update toolchain to version 6.1

--- src/bash/generate-destination-json.sh
+++ src/bash/generate-destination-json.sh
@@ -8,21 +8,21 @@ cat <<JSON > $SELF_DIR/$TARGET.json
     "version": 1,
     "dynamic-library-extension": "so",
     "toolchain-bin-dir": "$SWIFT_ANDROID_HOME/toolchain/usr/bin",
-    "sdk": "$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/darwin-x86_64/sysroot",
+    "sdk": "$SWIFT_ANDROID_HOME/toolchain",
     "target": "$TARGET",
     
     "extra-cc-flags": [
         "-fPIC",
         "-I$SWIFT_ANDROID_HOME/toolchain/usr/lib/swift-$SWIFT_ANDROID_ARCH",
-        "-I$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/darwin-x86_64/sysroot/usr/include",
-        "-I$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/darwin-x86_64/sysroot/usr/include/$TRIPLE"
+        "-I$SWIFT_ANDROID_HOME/toolchain/usr/include",
+        "-I$SWIFT_ANDROID_HOME/toolchain/usr/include/$TRIPLE"
     ],
     "extra-swiftc-flags": [
         "-use-ld=lld",
         "-resource-dir", "$SWIFT_ANDROID_HOME/toolchain/usr/lib/swift-$SWIFT_ANDROID_ARCH",
         "-tools-directory", "$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/darwin-x86_64/bin",
-        "-I$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/darwin-x86_64/sysroot/usr/include",
-        "-I$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/darwin-x86_64/sysroot/usr/include/$TRIPLE"
+        "-I$SWIFT_ANDROID_HOME/toolchain/usr/include",
+        "-I$SWIFT_ANDROID_HOME/toolchain/usr/include/$TRIPLE"
     ],
     "extra-cpp-flags": [
         "-lstdc++"
