02bf5ac473def22cc98a960a3981769020ebec83
Andrew Druk <adruk@readdle.com>
2025-05-10 20:27:52 +0300

    Dev: update path for new folder structure

--- src/bash/generate-destination-json.sh
+++ src/bash/generate-destination-json.sh
@@ -13,13 +13,13 @@ cat <<JSON > $SELF_DIR/$TARGET.json
     
     "extra-cc-flags": [
         "-fPIC",
-        "-I$SWIFT_ANDROID_HOME/toolchain/usr/lib/swift-$SWIFT_ANDROID_ARCH",
+        "-I$SWIFT_ANDROID_HOME/toolchain/usr/lib/swift",
         "-I$SWIFT_ANDROID_HOME/toolchain/usr/include",
         "-I$SWIFT_ANDROID_HOME/toolchain/usr/include/$TRIPLE"
     ],
     "extra-swiftc-flags": [
         "-use-ld=lld",
-        "-resource-dir", "$SWIFT_ANDROID_HOME/toolchain/usr/lib/swift-$SWIFT_ANDROID_ARCH",
+        "-resource-dir", "$SWIFT_ANDROID_HOME/toolchain/usr/lib/swift",
         "-tools-directory", "$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/darwin-x86_64/bin",
         "-I$SWIFT_ANDROID_HOME/toolchain/usr/include",
         "-I$SWIFT_ANDROID_HOME/toolchain/usr/include/$TRIPLE"
